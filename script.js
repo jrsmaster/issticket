5f1367f3294bae0989b799f96630f049c58c6148
jrsmaster <jrsmaster@gmail.com>
2024-07-08 00:35:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+            const url ='https://script.google.com/macros/s/AKfycbz6uSPBzkvBrb9Aevt7Uy_3JBiiibDYHzDINpO71YMecLxh0gLv3zSXQiwUstg7U33h1w/exec' ;
+            document.getElementById('form').action = url;
+            
+     
